226a2762e675d8c018388106000db08433009a3e
mhshifat <mehedihassanshifat01@gmail.com>
2026-02-12 03:31:43 +0600

    fix(provider): update TRPC client URL to use environment variable

--- src/trpc/provider.tsx
+++ src/trpc/provider.tsx
@@ -11,7 +11,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: "http://localhost:3000/api/trpc",
+                    url: `${process.env.APP_URL}/api/trpc`,
                 }),
             ],
         })
