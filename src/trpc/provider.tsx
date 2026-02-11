44a462fd066cc0a5a19038452cf02b48353426c0
mhshifat <mehedihassanshifat01@gmail.com>
2026-02-12 03:38:53 +0600

    fix(provider): update TRPC client URL to use relative path

--- src/trpc/provider.tsx
+++ src/trpc/provider.tsx
@@ -11,7 +11,7 @@ export function TRPCProvider({ children }: { children: React.ReactNode }) {
         trpc.createClient({
             links: [
                 httpBatchLink({
-                    url: `${process.env.APP_URL}/api/trpc`,
+                    url: `/api/trpc`,
                 }),
             ],
         })
